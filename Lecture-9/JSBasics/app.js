0b8aee10d980c0999e8fe79f971a8ac1225f5d18
Sabeel Khan <sabeelhps@gmail.com>
2021-07-03 22:07:23 +0530

    Lecture-9

---
+++ Lecture-9/JSBasics/app.js
@@ -0,0 +1,16 @@
+
+//This is comment
+
+let firstName = "Sabeel";
+let lastName = "Khan";
+
+let a = 15;
+let b = 20;
+
+// console.log('Sum of a and b is : ', a + b);
+// console.log(`Sum of a and b is ${a + b}`);
+
+// console.log(`Mr.${lastName},${firstName}`);
+
+
+// let name = "Sabeel"; let magicNumber;
