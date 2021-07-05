d8bcbbcc30fe25957e755719f3531755e8ab2de3
Sabeel Khan <sabeelkhan@Mosinas-MacBook-Pro.local>
2021-07-05 08:11:44 +0530

    Lecture-10

---
+++ Lecture-10/DecisionMaking/app.js
@@ -0,0 +1,30 @@
+let a=10;
+let b=99;
+
+// if(a>b){
+//     console.log('a is greater');
+// }
+
+// if(b>a){
+//     console.log("b is greater");
+// }
+
+// if(a>b){
+//     console.log('a is greater');
+// }
+// else{
+//     console.log('b is greater');
+// }
+
+
+// const age = parseInt(prompt("Enter your age")) 
+
+// if(age<18){
+//     alert("You cannot enter");
+// }
+// else if(age>=18 && age<25){
+//     alert("You can enter ,but u cannot drink");
+// }
+// else{
+//     alert("You can enterr and drink");
+// }
