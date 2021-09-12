1c2b76ce3d007d19eecd72619a6cbbe536f24920
Sabeel Khan <sabeelhps@gmail.com>
2021-09-12 20:27:14 +0530

    Lecture-28

---
+++ Lecture-28/E-Commerce-v2/models/product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+
+
+const productSchema = new mongoose.Schema({
+
+    name: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    img: {
+        type: String,
+        trim: true
+    },
+    price: {
+        type: Number,
+        min: 0,
+        default: 0
+    },
+    desc: {
+        type: String,
+        trim: true,
+    }
+});
+
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
